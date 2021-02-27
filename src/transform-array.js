3e3af4414a8d082d5a79e98d79bf25c32824f5dc
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-02-27 15:46:43 +0300

    feat: add function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error("Error");
+  }else{
+
+    let mass = arr.slice();
+    for(let i = 0; i < mass.length; i++){
+        if(mass[i] == "--discard-next" && mass[i+1] != null){
+            mass[i] = mass[i+1] = "del";
+        }else if(mass[i] == "--discard-prev" && mass[i-1] != null){
+            mass[i] = mass[i-1] = "del";
+        }else if(mass[i] == "--double-next" && mass[i+1] != null){
+            mass[i] = mass[i+1];
+        }else if(mass[i] == "--double-prev" && mass[i-1] != null){
+            mass[i] = mass[i-1];
+        }
+    }
+    let itog = [];
+    for(i = 0; i<mass.length; i++){
+        if(mass[i] != "del" && mass[i] != "--discard-next" && mass[i] != "--discard-prev" && mass[i] != "--double-next" && mass[i] != "--double-prev"){
+            itog.push(mass[i]);
+        }
+    } 
+    return itog;  
+  }
+} 
