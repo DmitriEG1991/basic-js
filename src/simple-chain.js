64d701458e1daffd9684840335b93efb8f41e84c
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-02-28 18:35:31 +0300

    feat: add function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  mass: [],
+  getLength(){
+    return this.mass.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value){
+    this.mass.push("( " + value + " )");
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof(position) != "number" || !Number.isInteger(position) || position > this.mass.length || position < 0){
+      this.mass = [];
+      throw new Error("Error");
+    }
+    this.mass.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.mass.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let mass2 = this.mass.slice().join("~~");
+    this.mass = [];
+    return mass2;
+  },
 };
 
 module.exports = chainMaker;
