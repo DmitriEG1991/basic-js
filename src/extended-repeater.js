4830b798a1ba90730f09941c4ee370a937da89e4
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-03-02 21:59:50 +0300

    feat: add function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  str = String(str);         
+  let a, b, c, d, e;
+  if(options.hasOwnProperty("repeatTimes")){
+    a = Number(options.repeatTimes);
+  }else{
+    a = 0;
+  }
+  if(options.hasOwnProperty("separator")){
+    b = String(options.separator);
+  }else{
+    b = "+";
+  }
+  if(options.hasOwnProperty("addition")){
+    c = String(options.addition);
+  }else{
+    c = "";
+  }
+  if(options.hasOwnProperty("additionRepeatTimes")){
+    d = Number(options.additionRepeatTimes);
+  }else{
+    d = 0;
+  }
+  if(options.hasOwnProperty("additionSeparator")){
+    e = String(options.additionSeparator);
+  }else{
+    e = "|";
+  }
+  let str2 = [];
+  str2.push(str);
+  str2.push(c);
+  for(let i =1; i<d; i++){
+    str2.push(e);
+    str2.push(c);
+  }        
+  for(let j=1; j<a; j++){
+    str2.push(b);
+    str2.push(str);
+    str2.push(c);
+    for(let i =1; i<d; i++){
+      str2.push(e);
+      str2.push(c);
+    }
+  }           
+  str2 = str2.join('');      
+  return str2;   
+}
