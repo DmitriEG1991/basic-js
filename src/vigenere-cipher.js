c2dd034d44b299aacc7abd72412fb81394a2d851
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-03-05 18:50:27 +0300

    feat: add function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,70 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.value = value;
+  }
+  encrypt(n, k) {
+    let n1 = n.toUpperCase();
+    let k1 = k.toUpperCase();
+    let n2 = n1.split("");
+    let k2 = k1.split("");
+    let mass = [];
+    for(let i = 0, k = 0; i < n2.length; i++){
+      let a1 = n2[i].charCodeAt();
+      if(a1 > 64 && a1 < 91){
+        if(!k2[k]){
+          k = 0;
+        }
+        let a2 = k2[k].charCodeAt();
+        let a3 = (a1-65) + (a2-65) + 65;
+        k++;
+        if(a3>90){
+          a3 = a3-26;
+        }
+        let a4 = String.fromCharCode(a3);
+        mass.push(a4);
+      }else{
+        mass.push(n2[i]);
+      }  
+    }
+    let str = mass.join("");
+    if (this.value == true || typeof this.value == "undefined") {
+      return str;
+    }else if (this.value == false) {
+      return str.split("").reverse().join("");
+    }
+  }
+  decrypt(n, k) {
+    let n1 = n.toUpperCase();
+    let k1 = k.toUpperCase();
+    let n2 = n1.split("");
+    let k2 = k1.split("");
+    let mass = [];  
+    for(let i = 0, k = 0; i < n2.length; i++){
+      let a1 = n2[i].charCodeAt();
+      if(a1 > 64 && a1 < 91){
+        if(!k2[k]){
+          k = 0;
+        }
+        let a2 = k2[k].charCodeAt();
+        let a3 = (a1-a2) + 65; 
+        k++;
+        if(a3<65){
+          a3 = a3+26;
+        }
+        let a4 = String.fromCharCode(a3);
+        mass.push(a4);
+      }else{
+        mass.push(n2[i]);
+      }  
+    }
+    let str = mass.join("");
+    if (this.value == true || typeof this.value == "undefined") {
+      return str;
+    }else if (this.value == false) {
+      return str.split("").reverse().join("");
+    } 
   }
 }
 
